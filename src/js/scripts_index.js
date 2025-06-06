1657ae63c5a5c6036e9c423f482b5a495929dcba
Erick Tapia <ericktapia09062006@gmail.com>
2025-06-06 02:55:22 -0600

    Add files via upload

---
+++ src/js/scripts_index.js
@@ -0,0 +1,26 @@
+//direccionamiento a tipos de maquinados
+document.addEventListener("DOMContentLoaded", function () { 
+    // Seleccionar el botón por su ID correcto
+    const btnMaquinados = document.getElementById("btn_maquinados"); 
+
+    const btnTratamientos = document.getElementById("btn_tratamienros");
+
+    const btnInicio = document.getElementById("btn_inicio");
+    if (btnMaquinados) {
+        btnMaquinados.addEventListener("click", function () {
+            window.location.href = "MAQUINADOS_2_tipoMaquinados.html";
+        });
+    }
+    if (btnTratamientos){
+        btnTratamientos.addEventListener("click", function(){
+            window.location.href = "tratamientos_termicos.html";
+        });
+    }
+if (btnInicio) {
+  btnInicio.addEventListener("click", function () {
+    window.location.href = "InicioSesion.html";
+  });
+}
+
+});
+
