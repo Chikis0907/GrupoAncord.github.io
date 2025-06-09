83d59cb31d35e33c516d31264d4428784f0c1e4e
Erick Tapia <ericktapia09062006@gmail.com>
2025-06-09 17:33:44 -0600

    Add files via upload

---
+++ src/php/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost"; // o IP del servidor
+$usuario = "root";   // tu usuario de MySQL
+$contrasena = "";    // tu contraseña de MySQL
+$base_datos = "gro_ancor";
+
+$conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+// Opcional: Forzar UTF-8
+//$c$conexion->set_charset("utf8");
+?>
